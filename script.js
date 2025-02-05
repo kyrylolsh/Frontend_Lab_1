a11d63cb70e0e87c7a050d6be9336adb6f6b5435
DashaDerpach <zipz231_ddv@student.ztu.edu.ua>
2025-02-05 14:29:40 +0200

    виправлено issue 3

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ const paragraphs = document.querySelectorAll('p');
 
 //
 document.body.style.fontFamily = 'Arial';
-
+//
 const cells = document.querySelectorAll('td');
-
 cells.forEach((cell, index) => {
-    if (cells.length > 0) {
-        cells.forEach((cell, index) => {
-            if (index % 2 === 0) {
-                cell.classList.add('selected');
-            }
-        });
+    if (index % 2 === 0) {
+        cell.classList.add('selected');
     }
 });
